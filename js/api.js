e68a938bfd6e4d2cec10feb19f549bfcfbf6ba15
mrjustindung <mrjustindung@gmail.com>
2026-06-21 05:05:39 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -19,3 +19,62 @@ async function getTodayFixtures() {
   const data = await res.json();
   return data.response || [];
 }
+async function loadMatches() {
+
+  const container = document.querySelector(".bottom");
+
+  try {
+
+    const fixtures = await getTodayFixtures();
+
+    container.innerHTML =
+      "<h2>TODAY'S MATCHES</h2>";
+
+    if (!fixtures.length) {
+
+      container.innerHTML += `
+      <div class="match">
+        No qualifiers today
+      </div>
+      `;
+
+      return;
+    }
+
+    fixtures.slice(0,5).forEach(match => {
+
+      const home =
+        match.teams.home.name;
+
+      const away =
+        match.teams.away.name;
+
+      const time =
+        new Date(match.fixture.date)
+        .toLocaleTimeString([],{
+          hour:'2-digit',
+          minute:'2-digit'
+        });
+
+      container.innerHTML += `
+        <div class="match">
+          <span>${home} vs ${away}</span>
+          <strong>${time}</strong>
+        </div>
+      `;
+    });
+
+  } catch(err) {
+
+    console.error(err);
+
+    container.innerHTML = `
+      <h2>TODAY'S MATCHES</h2>
+      <div class="match">
+        API ERROR
+      </div>
+    `;
+  }
+}
+
+loadMatches();
